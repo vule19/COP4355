fb47bf4cc7b3280d26648f0d8f151d00f5147812
vule19 <anhvle1901@gmail.com>
2025-04-18 22:17:07 -0400

    add node and Huffman files

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout << "Enter the text that you want to encode:" << endl;
+    string inputText;
+    cin >> inputText;
+    return 0;
+}
