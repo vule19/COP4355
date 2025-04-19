df7070e7cf84b0530d98c94e7de4dd2fa3160955
vule19 <anhvle1901@gmail.com>
2025-04-18 22:24:39 -0400

    add node

---
+++ HuffmanTree.cpp
@@ -0,0 +1,5 @@
+#include "node.cpp";
+
+class HuffmanTree {
+    Node* root;
+};
--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <string>
+#include <map>
+
+#include "node.cpp"
+#include "HuffmanTree.cpp"
 
 using namespace std;
+
+map<char, int> buildCodeMap(string inputText) {
+    map<char, int> codeMap;
+    for (char i : inputText) {
+        codeMap[i]++;
+    }
+    return codeMap;
+}
 
 int main() {
     cout << "Enter the text that you want to encode:" << endl;
     string inputText;
     cin >> inputText;
+
+    map<char, int> codeMap = buildCodeMap(inputText);
     return 0;
 }
