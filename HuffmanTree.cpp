b28f035679350f906848800f687bbb73a6d34743
vule19 <anhvle1901@gmail.com>
2025-04-18 23:03:01 -0400

    update gitignore

--- HuffmanTree.cpp
+++ HuffmanTree.cpp
@@ -1,5 +1,31 @@
 #include "node.cpp";
 
+#include <map>
+#include <string>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
 class HuffmanTree {
     Node* root;
+    map<char, int> codeMap;
+
+    HuffmanTree(map<char, int> codeMap_) {
+        codeMap = codeMap_;
+    }
+
+    struct Compare {
+        bool operator()(Node* n1, Node* n2) {
+            return n1->freq > n2->freq;
+        }
+    };
+
+    void buildMinHeap() {
+        priority_queue<Node*, vector<Node*>, Compare> pq;
+        for (int i = 0; i < codeMap.size(); i++) {
+            Node* newNode = new Node(codeMap[i].first, codeMap[i].second);
+            pq.push(newNode);
+        }
+    }
 };
